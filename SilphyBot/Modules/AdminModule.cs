7d48a142a133a0d959c08afffea6c096395a4ca6
Carbon <calumgcoppack@gmail.com>
2018-04-18 17:55:59 -0400

    trying to fix some things

---
+++ SilphyBot/Modules/AdminModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+
+namespace SilphyBot.Modules
+{
+    [Name("Admin")]
+    class AdminModule : ModuleBase<SocketCommandContext> {
+        [Command("ban")]
+        [Name("ban")]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task BanMemberAsync(ulong id, string reason = "") {
+            await Context.Guild.AddBanAsync(id);
+        }
+
+        [Command("ban")]
+        [Name("ban")]
+        [RequireBotPermission(GuildPermission.BanMembers)]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        public async Task BanMemberAsync(SocketGuildUser user) {
+            await Context.Guild.AddBanAsync(user);
+        }
+    }
+}
--- SilphyBot/Properties/Settings.Designer.cs
+++ SilphyBot/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace SilphyBot.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool testMode {
             get {
                 return ((bool)(this["testMode"]));
